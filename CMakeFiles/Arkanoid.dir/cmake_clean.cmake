e4c717cfad89d89ed04128c442378824d37afa54
Zinouo <znd.odn@gmail.com>
2024-03-06 14:23:44 +0100

    eh nan reuf jtai menti elle est là la vraie peufra tu vas péter ton crâne reuf t es pas prêt

---
+++ CMakeFiles/Arkanoid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Arkanoid"
+  "Arkanoid.pdb"
+  "CMakeFiles/Arkanoid.dir/arkanoid.c.o"
+  "CMakeFiles/Arkanoid.dir/arkanoid.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Arkanoid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

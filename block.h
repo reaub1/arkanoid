45a39032059e399def3e69eed6a5a065e12a1468
Robin <robin.bechlem@gmail.com>
2024-05-29 11:48:48 +0200

    power up

--- block.h
+++ block.h
@@ -4,90 +4,156 @@
 #include "SDL.h"
 #include "constants.h"
 
-SDL_Rect redbrick;
-SDL_Rect orangebrick;
-SDL_Rect whitebrick;
-SDL_Rect bluebrick;
-SDL_Rect bluelightbrick;
-SDL_Rect greenbrick;
-SDL_Rect navybrick;
-SDL_Rect darkgreenbrick;
-SDL_Rect pinkbrick;
-SDL_Rect yellowbrick;
-SDL_Rect darkredbrick;
-SDL_Rect magentabrick;
+extern SDL_Rect redbrick;
+extern SDL_Rect orangebrick;
+extern SDL_Rect whitebrick;
+extern SDL_Rect bluebrick;
+extern SDL_Rect bluelightbrick;
+extern SDL_Rect greenbrick;
+extern SDL_Rect navybrick;
+extern SDL_Rect darkgreenbrick;
+extern SDL_Rect pinkbrick;
+extern SDL_Rect yellowbrick;
+extern SDL_Rect darkredbrick;
+extern SDL_Rect magentabrick;
 
-SDL_Rect silverbrick1;
-SDL_Rect silverbrick2;
-SDL_Rect silverbrick3;
-SDL_Rect silverbrick4;
-SDL_Rect silverbrick5;
-SDL_Rect silverbrick6;
+extern SDL_Rect silverbrick1;
+extern SDL_Rect silverbrick2;
+extern SDL_Rect silverbrick3;
+extern SDL_Rect silverbrick4;
+extern SDL_Rect silverbrick5;
+extern SDL_Rect silverbrick6;
 
-SDL_Rect goldbrick1;
-SDL_Rect goldbrick2;
-SDL_Rect goldbrick3;
-SDL_Rect goldbrick4;
-SDL_Rect goldbrick5;
-SDL_Rect goldbrick6;
+extern SDL_Rect goldbrick1;
+extern SDL_Rect goldbrick2;
+extern SDL_Rect goldbrick3;
+extern SDL_Rect goldbrick4;
+extern SDL_Rect goldbrick5;
+extern SDL_Rect goldbrick6;
 
-SDL_Rect ball1;
-SDL_Rect ball2;
-SDL_Rect ball3;
-SDL_Rect ball4;
-SDL_Rect ball5;
-SDL_Rect ball6;
+extern SDL_Rect ball1;
+extern SDL_Rect ball2;
+extern SDL_Rect ball3;
+extern SDL_Rect ball4;
+extern SDL_Rect ball5;
+extern SDL_Rect ball6;
 
-SDL_Rect background1;
-SDL_Rect background2;
-SDL_Rect background3;
-SDL_Rect background4;
-SDL_Rect background5;
-SDL_Rect background6;
+extern SDL_Rect background1;
+extern SDL_Rect background2;
+extern SDL_Rect background3;
+extern SDL_Rect background4;
+extern SDL_Rect background5;
+extern SDL_Rect background6;
 
-SDL_Rect darkbackground1;
-SDL_Rect darkbackground2;
-SDL_Rect darkbackground3;
-SDL_Rect darkbackground4;
-SDL_Rect darkbackground5;
-SDL_Rect darkbackground6;
+extern SDL_Rect darkbackground1;
+extern SDL_Rect darkbackground2;
+extern SDL_Rect darkbackground3;
+extern SDL_Rect darkbackground4;
+extern SDL_Rect darkbackground5;
+extern SDL_Rect darkbackground6;
 
-SDL_Rect a;
-SDL_Rect b;
-SDL_Rect c;
-SDL_Rect d;
-SDL_Rect e;
-SDL_Rect f;
-SDL_Rect g;
-SDL_Rect h;
-SDL_Rect i;
-SDL_Rect j;
-SDL_Rect k;
-SDL_Rect l;
-SDL_Rect m;
-SDL_Rect n;
-SDL_Rect o;
-SDL_Rect p;
-SDL_Rect q;
-SDL_Rect r;
-SDL_Rect s;
-SDL_Rect t;
-SDL_Rect u;
-SDL_Rect v;
-SDL_Rect w;
-SDL_Rect x;
-SDL_Rect y;
-SDL_Rect z;
+extern SDL_Rect a;
+extern SDL_Rect b;
+extern SDL_Rect c;
+extern SDL_Rect d;
+extern SDL_Rect e;
+extern SDL_Rect f;
+extern SDL_Rect g;
+extern SDL_Rect h;
+extern SDL_Rect i;
+extern SDL_Rect j;
+extern SDL_Rect k;
+extern SDL_Rect l;
+extern SDL_Rect m;
+extern SDL_Rect n;
+extern SDL_Rect o;
+extern SDL_Rect p;
+extern SDL_Rect q;
+extern SDL_Rect r;
+extern SDL_Rect s;
+extern SDL_Rect t;
+extern SDL_Rect u;
+extern SDL_Rect v;
+extern SDL_Rect w;
+extern SDL_Rect x;
+extern SDL_Rect y;
+extern SDL_Rect z;
 
-SDL_Rect zero;
-SDL_Rect un;
-SDL_Rect deux;
-SDL_Rect trois;
-SDL_Rect quatre;
-SDL_Rect cinq;
-SDL_Rect six;
-SDL_Rect sept;
-SDL_Rect huit;
-SDL_Rect neuf;
+extern SDL_Rect zero;
+extern SDL_Rect un;
+extern SDL_Rect deux;
+extern SDL_Rect trois;
+extern SDL_Rect quatre;
+extern SDL_Rect cinq;
+extern SDL_Rect six;
+extern SDL_Rect sept;
+extern SDL_Rect huit;
+extern SDL_Rect neuf;
+
+extern SDL_Rect slow1;
+extern SDL_Rect slow2;
+extern SDL_Rect slow3;
+extern SDL_Rect slow4;
+extern SDL_Rect slow5;
+extern SDL_Rect slow6;
+extern SDL_Rect slow7;
+extern SDL_Rect slow8;
+
+extern SDL_Rect catch1;
+extern SDL_Rect catch2;
+extern SDL_Rect catch3;
+extern SDL_Rect catch4;
+extern SDL_Rect catch5;
+extern SDL_Rect catch6;
+extern SDL_Rect catch7;
+extern SDL_Rect catch8;
+
+extern SDL_Rect expand1;
+extern SDL_Rect expand2;
+extern SDL_Rect expand3;
+extern SDL_Rect expand4;
+extern SDL_Rect expand5;
+extern SDL_Rect expand6;
+extern SDL_Rect expand7;
+extern SDL_Rect expand8;
+
+extern SDL_Rect divide1;
+extern SDL_Rect divide2;
+extern SDL_Rect divide3;
+extern SDL_Rect divide4;
+extern SDL_Rect divide5;
+extern SDL_Rect divide6;
+extern SDL_Rect divide7;
+extern SDL_Rect divide8;
+
+extern SDL_Rect laser1;
+extern SDL_Rect laser2;
+extern SDL_Rect laser3;
+extern SDL_Rect laser4;
+extern SDL_Rect laser5;
+extern SDL_Rect laser6;
+extern SDL_Rect laser7;
+extern SDL_Rect laser8;
+
+extern SDL_Rect break1;
+extern SDL_Rect break2;
+extern SDL_Rect break3;
+extern SDL_Rect break4;
+extern SDL_Rect break5;
+extern SDL_Rect break6;
+extern SDL_Rect break7;
+extern SDL_Rect break8;
+
+extern SDL_Rect player1;
+extern SDL_Rect player2;
+extern SDL_Rect player3;
+extern SDL_Rect player4;
+extern SDL_Rect player5;
+extern SDL_Rect player6;
+extern SDL_Rect player7;
+extern SDL_Rect player8;
+
+
+
 
 #endif
